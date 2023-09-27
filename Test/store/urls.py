a0e914165a3eb9dcab9658462431943ce2cc5520
NPython1 <142802547+NPython1@users.noreply.github.com>
2023-09-27 13:48:11 +0530

    urls.py

--- Test/store/urls.py
+++ Test/store/urls.py
@@ -8,6 +8,7 @@ app_name:'store'
 urlpatterns = [
     path('', views.index, name="index"),
     path('member/add/', views.create_view, name='add'),
+     path('member/add/messages',views.messages,name='messages'),
     path('member/<int:pk>/', views.update_view, name='change'),
 
     path('member/ajax/load_courses/', views.load_courses, name='ajax_load_courses'),
